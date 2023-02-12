bf333e0c17bff0a05ce5ac45bb9d3b3d2efbc7dd
Dmitriy <dmtr.osipenko@gmail.com>
2023-02-12 20:52:50 +0300

    feat: class component

--- src/components/ClassComponent2/ClassComponent2.jsx
+++ src/components/ClassComponent2/ClassComponent2.jsx
@@ -36,11 +36,12 @@ class ClassComponent2 extends Component {
   render() {
     return (
       <div style={{ padding: "50px" }}>
-        <h1>Hello, {this.props.name}!</h1> // todo this.props -> this.state
+        <h1>Hello, {this.props.name}!</h1>
         <h1>age: {this.state.age}!</h1>
         {/*<button onClick={this.handleClick}>Click</button>*/}
         <div style={{ padding: "5px" }}>
           <NavLink to="/">Back to Dima</NavLink>
+          <br />
           <br />
           <NavLink to="/oleg">Go to Oleg</NavLink>
         </div>
